b2dce07c437ad27fb40f0c75bbe4defb20137615
Najum Ali <n.ali@isys-software.de>
2014-04-03 23:57:21 +0200

    Added property file for choosing csv data, deleted unused stuff, footer is not fixed now

---
+++ src/main/java/de/jstage/recommender/cf/config/MahoutDataModelConfig.java
@@ -0,0 +1,47 @@
+package de.jstage.recommender.cf.config;
+
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.core.env.Environment;
+
+import javax.inject.Inject;
+import javax.servlet.ServletContext;
+import java.io.File;
+import java.io.IOException;
+
+@Configuration
+@PropertySources(value = {@PropertySource("/mahoutConfig.properties")})
+public class MahoutDataModelConfig {
+
+	@Inject
+	private ServletContext servletContext;
+
+	@Inject
+	private Environment environment;
+
+	@Bean(name = "fileDataModel")
+	public DataModel fileDataModel() throws IOException {
+		final String filePath = servletContext.getRealPath("/resources/preference_csv/");
+		final String fileName = environment.getProperty("dataModelFile");
+		return new FileDataModel(new File(filePath + fileName));
+	}
+
+//	@Bean
+//	public DataSource dataSource() {
+//		BasicDataSource basicDataSource = new BasicDataSource();
+//		basicDataSource.setDriverClassName(DATABASE_DRIVER);
+//		basicDataSource.setUrl(DATABASE_URL);
+//		basicDataSource.setPassword(DATABASE_PASSWORD);
+//		basicDataSource.setUsername(DATABASE_USERNAME);
+//		return basicDataSource;
+//	}
+//
+//	@Bean
+//	public JdbcTemplate jdbcTemplate() {
+//		return new JdbcTemplate(dataSource());
+//	}
+}
