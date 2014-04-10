c40d77de364081a952dd1bf0e5d4d3614ce973aa
Najum Ali <n.ali@isys-software.de>
2014-04-10 15:44:14 +0200

    update mahout to 0.9 adopted JVM parameters from Mahout in Action

--- jettyRunDebug.sh
+++ jettyRunDebug.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-export GRADLE_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=9999,server=y,suspend=n"
+export GRADLE_OPTS="-server -d64 -Xmx2048m -XX:+UseParallelGC -XX:+UseParallelOldGC -Xdebug -Xrunjdwp:transport=dt_socket,address=9999,server=y,suspend=n"
 sh gradlew jettyRun --no-daemon -Dorg.gradle.debug=true
